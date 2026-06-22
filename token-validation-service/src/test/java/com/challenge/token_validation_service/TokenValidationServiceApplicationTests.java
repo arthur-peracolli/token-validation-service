38806876f67bd4f968055bc1827d35475eadd9a2
arthur-peracolli <arthurperacolli@hotmail.com>
2026-06-22 20:41:36 -0300

    innitial project commit

---
+++ token-validation-service/src/test/java/com/challenge/token_validation_service/TokenValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.token_validation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
